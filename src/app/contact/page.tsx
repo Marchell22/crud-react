77d02e564e8323183ebe0adf9ae4a72285597fd1
Marchell22 <ramotmanurung424@gmail.com>
2025-02-28 18:00:46 +0700

    update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,14 @@
 import ContactTable from "@/components/contact-table";
 import Search from "@/components/search";
+import {CreateButton} from "@/components/button"
 const contacts = () => {
   return (
     <div className="max-w-screen-md mx-auto mt-5">
-      <div className="flex items-center justify-between mb-5 text-sm outline-2 rounded-sm" aria-placeholder="Search..."><Search></Search></div>
-      <ContactTable/>
+      <div className="flex items-center justify-between gap-1 mb-5 text-sm outline-2 rounded-sm">
+        <Search />
+        <CreateButton/>
+      </div>
+      <ContactTable />
     </div>
   );
 };
