88fa353793db2d84e4aa61957a4418872e61cae5
Marchell22 <ramotmanurung424@gmail.com>
2025-03-28 01:54:50 +0700

    Pagination

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,17 +1,26 @@
 import ContactTable from "@/components/contact-table";
+import Pagination from '@/components/pagination';
 import Search from "@/components/search";
 import {CreateButton} from "@/components/button"
-const contacts = ({searchParams}:{searchParams?:{ query?:string; page?:string;}
+import { getContactsPages } from "../../../lib/data";
+
+
+
+const contacts = async ({searchParams}:{searchParams?:{ query?:string; page?:string;}
 }) => {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await getContactsPages(query)
   return (
     <div className="max-w-screen-md mx-auto mt-5">
       <div className="flex items-center justify-between gap-1 mb-5 text-sm outline-2 rounded-sm">
         <Search />
-        <CreateButton/>
+        <CreateButton />
       </div>
-      <ContactTable query={query} currentPage={currentPage}/>
+      <ContactTable query={query} currentPage={currentPage} />
+      <div className="flex justify-center mt-4">
+        <Pagination totalPages={totalPages}></Pagination>
+      </div>
     </div>
   );
 };
